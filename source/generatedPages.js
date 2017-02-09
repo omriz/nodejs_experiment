ff6e077b579290d081fe5606e4da6c36259d3a15
Omri Zohar <omri.zohar@gmail.com>
2017-02-09 21:29:27 +0000

    Adding some static files and generated pages

---
+++ source/generatedPages.js
@@ -0,0 +1,10 @@
+// Declaring exposed methods.
+module.exports = {
+    pageOne: pageOne,
+};
+
+// Implemntation details.
+function pageOne(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Page One');
+}
