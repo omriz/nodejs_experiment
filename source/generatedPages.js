3ca8d0d826bf54abf4b7017eafc37ce21f2011f9
Omri Zohar <omri.zohar@gmail.com>
2017-03-11 21:40:30 +0000

    Minor comments

--- source/generatedPages.js
+++ source/generatedPages.js
@@ -2,15 +2,28 @@
 // Imports.
 const Handlebars = require('handlebars');
 
+// Constants.
+const TEMPLATE_ROOT = './templates';
+
 // Declaring exposed methods.
 module.exports = {
     PageRouter: PageRouter,
 };
 
-// Implemntation details.
+// PageRouter class.
+// TODO(omri): Add a loading for handlebars templates.
+// Constructor.
 function PageRouter() {
     this.dispatcher = null;
 }
+
+// Configuring routes.
+PageRouter.prototype.configureDispatcher = function(dispatcher) {
+    this.dispatcher = dispatcher;
+    this.dispatcher.onGet('/page1', this.pageOne);
+    this.dispatcher.onGet('/page2', this.pageTwo);
+};
+
 PageRouter.prototype.pageOne = function(req, res) {
     res.writeHead(200, {
         'Content-Type': 'text/plain'
@@ -26,10 +39,4 @@ PageRouter.prototype.pageTwo = function(req, res) {
     res.end(template({
         'myName': 'omri'
     }));
-};
-
-PageRouter.prototype.configureDispatcher = function(dispatcher) {
-    this.dispatcher = dispatcher;
-    this.dispatcher.onGet('/page1', this.pageOne);
-    this.dispatcher.onGet('/page2', this.pageTwo);
 };
